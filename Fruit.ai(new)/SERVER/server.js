5f918e27327fb615189eeb5ca180e03cbe0c886a
armankang <102130568+armankang@users.noreply.github.com>
2024-09-16 00:26:34 +0530

    Update server.js

--- Fruit.ai(new)/SERVER/server.js
+++ Fruit.ai(new)/SERVER/server.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 require('dotenv').config(); 
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = 5001;
 
 // CORS middleware
 // app.use(cors({
